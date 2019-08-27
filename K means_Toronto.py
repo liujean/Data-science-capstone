f2b0b96c858f53fa0e699f6acffcd82040064e7d
liujean <liujean@users.noreply.github.com>
2019-08-26 19:48:10 -0700

    Add files via upload

---
+++ K means_Toronto.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 00:44:01 2019
+
+@author: jean2
+"""
+
+# =============================================================================
+# Web Scraping Wikipedia Tables using BeautifulSoup and Python
+# =============================================================================
+
+from bs4 import beautifulsoup4
+import requests
+import pandas as pd
+
+
+# assign the link of the website to scrape the data
+website_url = requests.get('https://en.wikipedia.org/wiki/List_of_Asian_countries_by_area').text
+
+soup = BeautifulSoup (website_url,'lxml')
+print(soup.prettify())
+
+# all table contents are under class "wikitable sortable"
+My_table = soup.find('table',{'class':'wikitable sortable'})
+links = My_table.findAll('a')
+links
+
+Countries = []
+for link in links:
+    Countries.append(link.get('title'))
+print(Countries)
+
+df = pd.DataFrame()
+df['Country'] = Countries
+df
+
+
+# =============================================================================
+# K Means: segmenting neighborhoods in Toronto
+# =============================================================================
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+
+# assign the link of the website to scrape the data
+website_url = requests.get('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M').text
+
+soup = BeautifulSoup (website_url,'lxml')
+print(soup.prettify())
+
+# all table contents are under class "wikitable sortable"
+My_table = soup.find('table',{'class':'wikitable sortable'})
+table_rows = My_table.findAll('tr')
+
+data = []
+for row in table_rows:
+    data.append([t.text.strip() for t in row.find_all('td')])
+    
+df = pd.DataFrame(data, columns = ['PostalCode','Borough','Neighborhood'])
+
+df.head()
+df.info()
+df.shape()
+
+# ignore cells where Borough is "not assigned"
+df.drop(df[df['Borough']=="Not assigned"].index,axis=0, inplace=True)
+
+df1 = df.reset_index()
+df1.head()
+df1.info()
+
+df2 = df1.groupby('PostalCode').agg(lambda x: ','.join(x))
+df2.head()
+df2.info()
+
+df3 = df2.reset_index()
+df3['Borough']= 
+    df3['Borough'].str.replace('nan|[{}\s]','').str.split(',').apply(set).str.join(',').str.strip(',').str.replace(",{2,}",",")
+df3 = df2.reset_index()
+df3.head()
+
+df3.shape()
+
+
+
